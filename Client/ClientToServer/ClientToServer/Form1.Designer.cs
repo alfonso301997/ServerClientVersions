8fac8046163dec8e9c44750802329bae832fbb6a
unknown <alfonsobustamantegacayanjr@gmail.com>
2021-01-11 00:08:04 +0800

    first commit

---
+++ Client/ClientToServer/ClientToServer/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ClientToServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.message = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // message
+            // 
+            this.message.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.message.Location = new System.Drawing.Point(12, 12);
+            this.message.Multiline = true;
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(381, 226);
+            this.message.TabIndex = 0;
+          
+            // 
+            // submit
+            // 
+            this.submit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.submit.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(12, 244);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(381, 66);
+            this.submit.TabIndex = 1;
+            this.submit.Text = "SUBMIT";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 322);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Button submit;
+    }
+}
+
---
+++ Server/ClientServer/ClientServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ip, 8080);
+            TcpClient client = default(TcpClient);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server Started!...");
+                
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.ToString());
+                Console.Read();
+
+            }
+            while(true)
+                {
+                client = server.AcceptTcpClient();
+                byte[] receivedbuffer = new byte[100];
+                NetworkStream stream = client.GetStream();
+                stream.Read(receivedbuffer, 0  , receivedbuffer.Length);
+                StringBuilder msg = new StringBuilder();
+
+                foreach (byte b in receivedbuffer)
+                {
+                    if (b.Equals(00))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        msg.Append(Convert.ToChar(b).ToString());
+                    }
+                }
+                Console.WriteLine(msg.ToString() + msg.Length);
+                
+            }
+        }
+    }
+}
